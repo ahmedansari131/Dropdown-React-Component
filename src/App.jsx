4bf94e08532c8710a5e134e79e5012eac0b1d3f2
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2024-02-18 21:39:27 +0530

    feat: change the way of rendering the dropdown

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,29 @@
-import { FloatingDropdown } from "./components";
+import { DropdownContent, FloatingDropdown, Label, List } from "./components";
 import { DropdownProvider } from "./context/dropdownContext";
 import { useDropdownController } from "./hooks";
-import KeyboardArrowDown from "@mui/icons-material/KeyboardArrowDown";
 
 function App() {
   const { dropdownState, dropdownController } = useDropdownController();
 
-  const dropdownContent = [
-    {
-      name: "Home",
-      icon: <KeyboardArrowDown />,
-      listFunctionHandler: () => {
-        alert("Hello World");
-      },
-    },
-    {
-      name: "About",
-      icon: "",
-      listFunctionHandler: () => {},
-    },
-    {
-      name: "Service",
-      icon: "",
-      listFunctionHandler: () => {},
-    },
-    {
-      name: "Contact",
-      icon: "",
-      listFunctionHandler: () => {},
-    },
-  ];
-
   return (
     <DropdownProvider value={{ dropdownState, dropdownController }}>
       <div className="w-full h-screen bg-slate-900 flex justify-center items-center gap-5">
-        <FloatingDropdown
-          children={dropdownContent}
-          label={"Hover"}
-          hover={true}
-        />
-
-        <FloatingDropdown label={"Click"} children={dropdownContent} />
+        <FloatingDropdown label={"Dropdown"}>
+          <DropdownContent>
+            <List listName={"Home"} onClick={() => alert("Hello")} />
+            <List listName={"About"} />
+            <List listName={"Service"} />
+            <List listName={"Contact"} />
+          </DropdownContent>
+        </FloatingDropdown>
+        <FloatingDropdown label={"Dropdown"} hover={true}>
+          <DropdownContent>
+            <List listName={"Home"} />
+            <List listName={"About"} />
+            <List listName={"Service"} />
+            <List listName={"Contact"} />
+          </DropdownContent>
+        </FloatingDropdown>
       </div>
     </DropdownProvider>
   );
