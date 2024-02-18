7277ef9ab1fc8a2036335170ccf035d6936d6533
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2024-02-18 21:40:39 +0530

    feat: add list component

---
+++ src/components/list/List.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Icon from "../icon/Icon";
+import cn from "../../utils/cn";
+
+const List = ({ listName, icon, className = "", ...props }) => {
+  return (
+    <li
+      className={cn(
+        "text-gray-100 py-2 px-3 hover:bg-slate-800 text-sm flex items-center gap-3",
+        className
+      )}
+      {...props}
+    >
+      {icon && <Icon />}
+
+      {listName}
+    </li>
+  );
+};
+
+export default List;
