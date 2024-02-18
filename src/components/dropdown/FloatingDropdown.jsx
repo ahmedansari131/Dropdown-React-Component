57df97f0b5c36418d46b0932836114595703911e
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2024-02-18 21:40:14 +0530

    refactor: add dropdownRef and related functions

--- src/components/dropdown/FloatingDropdown.jsx
+++ src/components/dropdown/FloatingDropdown.jsx
@@ -5,16 +5,28 @@ import React, {
   useLayoutEffect,
   useRef,
 } from "react";
-import { DropdownContent, KeyboardArrowDownIcon } from "..";
+import { KeyboardArrowDownIcon, Label } from "..";
 import useDropdown from "../../context/dropdownContext";
-import clsx from "clsx";
+import cn from "../../utils/cn";
 
 const DropdownWrapper = (props) => {
-  const { label, children, hover = false } = props;
+  const { label, children, className, hover = false } = props;
   const generateId = useId();
   const { dropdownState, dropdownController } = useDropdown();
   const dropdownRef = useRef(null);
   const [positionY, setPositionY] = useState("top-full");
+
+  const mouseEnterHandler = () => {
+    if (hover) {
+      dropdownController(generateId, hover);
+    }
+  };
+
+  const mouseClickHandler = () => {
+    if (!hover) {
+      dropdownController(generateId, hover);
+    }
+  };
 
   const clickOutsideHandler = () => {
     window.addEventListener("click", (e) => {
@@ -35,36 +47,6 @@ const DropdownWrapper = (props) => {
     }
   };
 
-  const mouseEnterHandler = () => {
-    if (hover) {
-      dropdownController(generateId, hover);
-    }
-  };
-
-  const mouseLeaveHandler = (e) => {
-    if (
-      hover &&
-      dropdownRef.current &&
-      (!e.target.contains(dropdownRef.current) ||
-        e.target.contains(dropdownRef.current))
-    ) {
-      dropdownController(null);
-    }
-  };
-
-  const mouseClickHandler = () => {
-    if (!hover) {
-      dropdownController(generateId, hover);
-    }
-  };
-
-  const dropdownContent = {
-    defaultStyle:
-      "absolute left-0 w-full bg-slate-700 py-2 cursor-pointer rounded-md -z-10",
-    isOpen: "z-10 transition-all duration-300 ",
-    isClose: "h-0 -translate-y-6 overflow-hidden",
-  };
-
   useLayoutEffect(() => {
     adjustPosition();
   }, [dropdownState]);
@@ -73,17 +55,21 @@ const DropdownWrapper = (props) => {
     clickOutsideHandler();
   }, [dropdownRef.current]);
 
+  const dropdownContentStyle = {
+    defaultStyle: "absolute left-0 w-full cursor-pointer -z-10",
+    isOpen: "z-10 transition-all duration-300 ",
+    isClose: "h-0 -translate-y-6 overflow-hidden",
+  };
+
   return (
-    <div
-      className="text-gray-100 min-w-80 relative"
-      onMouseEnter={mouseEnterHandler}
-      onMouseLeave={mouseLeaveHandler}
-      onClick={mouseClickHandler}
-    >
-      <div className="w-full">
-        <div className="w-full border border-slate-600 cursor-pointer px-3 py-2 rounded-md flex items-center justify-between select-none">
-          {label || "Label"}
-          <span className="pointer-events-none relative ">
+    <>
+      <div className="w-full relative rounded-md">
+        <Label
+          className={className}
+          onMouseEnter={mouseEnterHandler}
+          onClick={mouseClickHandler}
+          label={label}
+          icon={
             <KeyboardArrowDownIcon
               style={{
                 transition: "transform 0.2s ease-in-out",
@@ -93,23 +79,25 @@ const DropdownWrapper = (props) => {
                     : "rotate(0deg)",
               }}
             />
-          </span>
-        </div>
-
-        <div className="h-2"></div>
+          }
+        />
+        <div data-element-gap="true" className="h-2"></div>
 
         <div
           ref={dropdownRef}
-          onMouseLeave={mouseLeaveHandler}
-          className={clsx(dropdownContent.defaultStyle, positionY, {
-            [dropdownContent.isOpen]: dropdownState === generateId,
-            [dropdownContent.isClose]: dropdownState !== generateId,
-          })}
+          className={cn(
+            dropdownContentStyle.defaultStyle,
+            positionY,
+            {
+              [dropdownContentStyle.isOpen]: dropdownState === generateId,
+              [dropdownContentStyle.isClose]: dropdownState !== generateId,
+            }
+          )}
         >
-          <DropdownContent list ={children} />
+          {children}
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
