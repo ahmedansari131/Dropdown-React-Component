a1e4a67df971796d05d61e56bdebeb8850051270
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2024-02-19 22:35:16 +0530

    feat: add context provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.jsx";
+import "./index.css";
+import { DropdownProvider } from "./context/dropdownContext.js";
+import useDropdownController from "./hooks/useDropdownController.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const Wrapper = () => {
+  // Use the useDropdownController hook here
+  const { dropdownState, dropdownController } = useDropdownController();
+
+  return (
+    <DropdownProvider value={{ dropdownState, dropdownController }}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </DropdownProvider>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(<Wrapper />);
