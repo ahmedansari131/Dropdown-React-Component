02ac1b3bdfdcde908f8c37f313e2f1b7ac5c34d3
Ahmed Ansari <ansarimohammadahmed49@gmail.com>
2024-02-19 22:36:36 +0530

    refactor: add floating dropdown component

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,21 @@
-import { DropdownContent, FloatingDropdown, Label, List } from "./components";
-import { DropdownProvider } from "./context/dropdownContext";
-import { useDropdownController } from "./hooks";
+import { DropdownContent, FloatingDropdown, List } from "./components";
 
 function App() {
-  const { dropdownState, dropdownController } = useDropdownController();
-
   return (
-    <DropdownProvider value={{ dropdownState, dropdownController }}>
-      <div className="w-full h-screen bg-slate-900 flex justify-center items-center gap-5">
-        <FloatingDropdown label={"Dropdown"}>
-          <DropdownContent>
-            <List listName={"Home"} onClick={() => alert("Hello")} />
-            <List listName={"About"} />
-            <List listName={"Service"} />
-            <List listName={"Contact"} />
-          </DropdownContent>
-        </FloatingDropdown>
-        <FloatingDropdown label={"Dropdown"} hover={true}>
-          <DropdownContent>
-            <List listName={"Home"} />
-            <List listName={"About"} />
-            <List listName={"Service"} />
-            <List listName={"Contact"} />
-          </DropdownContent>
-        </FloatingDropdown>
-      </div>
-    </DropdownProvider>
+    <>
+      <FloatingDropdown label={"Hello Dropdown"}>
+        <DropdownContent>
+          <List listName={"Home"} />
+          <List listName={"Home"} />
+          <List listName={"Home"} />
+          <List listName={"Home"} />
+          <List listName={"Home"} />
+          <List listName={"Home"} />
+          <List listName={"Home"} />
+          <List listName={"Home"} />
+        </DropdownContent>
+      </FloatingDropdown>
+    </>
   );
 }
 
